99312336f635ca7def5e77982e6a16200d62d223
MONSRC <eduardermakov769@gmail.com>
2025-11-11 02:03:56 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        FUNXPAY
+import os        MONSRC
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
